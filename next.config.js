1ecc4af47e4ee0c1276970d7fc3569adffb1b1f9
Ivo Pereira <ipereira@oddsscanner.com>
2025-05-13 10:20:50 +0100

    Update next.config to follow new standard

--- next.config.js
+++ next.config.js
@@ -8,8 +8,14 @@ const withNextIntl = createNextIntlPlugin({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['static-sandbox.oddscanner.xyz'],
-  },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static-sandbox.oddscanner.xyz',
+        pathname: '/**',
+      },
+    ]
+  }
 };
 
 module.exports = withNextIntl(nextConfig);
