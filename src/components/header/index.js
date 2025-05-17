b865bb76b65f4cffb191c5079c1c724ca1934690
Ivo Pereira <ipereira@oddsscanner.com>
2025-05-17 01:38:53 +0100

    Replace <a> with Next/Link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 
 export default function Header() {
@@ -9,6 +8,21 @@ export default function Header() {
         <div className="max-w-6xl mx-auto">
             <h1>Odds Scanner</h1>
             <nav>
+              <ul className="flex space-x-4">
+                <li><Link href="/">UK</Link></li>
+                <li><Link href="/pt">PT</Link></li>
+                <li><Link href="/es">ES</Link></li>
+                <li><Link href="/br">BR</Link></li>
+                <li><Link href="/us">US</Link></li>
+                <li><Link href="/fr">FR</Link></li>
+              </ul>
+            </nav>
+
+            <nav>
+              <ul className="flex space-x-4">
+                <li><Link href="/br/contato">contato</Link></li>
+                <li><Link href="/br/artigos">Artigos</Link></li>
+              </ul>
             </nav>
         </div>
     </header>
