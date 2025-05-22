b15cf6a4f41d22b6a993919a4d3e63718377b1c5
abe <abel.rogers@gmail.com>
2025-05-22 11:04:12 +0100

    preload fonts and have a fallback in case of problems

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -18,15 +18,16 @@ const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
-  // weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  // style: ['normal', 'italic'],
+  preload: true,
+  fallback: ['system-ui', 'arial'],
 });
 
 const sora = Sora({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-sora',
-  // weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  preload: true,
+  fallback: ['system-ui', 'arial'],
 });
 
 export default async function Layout(props) {
