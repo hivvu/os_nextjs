9ba39325e577952fb51e96560b146d0b5fb343c6
Ivo Pereira <ipereira@oddsscanner.com>
2025-05-22 11:45:40 +0100

    Update next.config for vercel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,12 +9,17 @@ const nextConfig = {
         hostname: 'static-sandbox.oddscanner.xyz',
         pathname: '/**',
       },
-    ]
+    ],
   },
   sassOptions: {
     includePaths: ['./src/styles'],
     silenceDeprecations: ['legacy-js-api'],
   },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
